d98addd26de0f0001015b353c589ab7408b8aa6e
ethankhoa <ethan.huynh@gmail.com>
2017-03-11 16:53:44 +1100

    changing project gallery back to original

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     @nav_array = [
       { title: 'Blog', icon: 'fa-rss', path: '/pages/blog', hover: 'hvr-pulse-grow' },
       { title: 'About', icon: 'fa-rebel', path: '/pages/about', hover: 'hvr-wobble-horizontal' },
-      { title: 'Contact', icon: 'fa-paper-plane', path: '/pages/contact', hover: 'hvr-pulse-grow' },
+      { title: 'Contact', icon: 'fa-paper-plane', path: '/pages/contact', hover: 'hvr-wobble-to-top-right' },
       { title: 'GitHub', icon: 'fa-github', path: 'https://www.github.com/ethankhoa', hover: 'hvr-wobble-horizontal' }
     ]
 
@@ -21,13 +21,13 @@ class PagesController < ApplicationController
 
   def projects
     @projects_array = [
-      { project_num: 's-1', project_name: 'ATM Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/800/600/technics', description: 'The first and the best ATM terminal ever made!', github_url: 'https://www.github.com', project_cat: 'Ruby'  },
-      { project_num: 's-2', project_name: 'Recipe and Google Places Restaurant Finder', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/600', description: 'Recipe finder! Yes!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
-      { project_num: 's-3', project_name: 'Weather Table and Progress Bar', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/600/food', description: 'Ugh, not another ATM app!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
-      { project_num: 's-4', project_name: 'ATM Terminal Part Deux', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/600/nature', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
-      { project_num: 's-5', project_name: 'ATM Terminal Part Deux', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/600/nature', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' }
+      { project_num: 's-1', project_name: 'ATM Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/800/800/technics', description: 'The first and the best ATM terminal ever made!', github_url: 'https://www.github.com', project_cat: 'Ruby'  },
+      { project_num: 's-2', project_name: 'Recipe and Google Places Restaurant Finder', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/800', description: 'Recipe finder! Yes!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
+      { project_num: 's-3', project_name: 'Weather Table and Progress Bar', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/800/food', description: 'Ugh, not another ATM app!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
+      { project_num: 's-4', project_name: 'ATM Terminal Part Deux', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/800/nature', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
+      { project_num: 's-5', project_name: 'ATM Terminal Part Deux', project_url: '/pages/projects', project_image: 'http://lorempixel.com/800/800/cats', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
+      { project_num: 's-1', project_name: 'ATM Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/800/800/technics', description: 'The first and the best ATM terminal ever made!', github_url: 'https://www.github.com', project_cat: 'Ruby'  }
                       ]
-
 
 end
 
