6230e354b93e6b2c69340baa2a1fafe631e86969
ethankhoa <ethan.huynh@gmail.com>
2017-03-13 13:02:56 +1100

    adjusted about icons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,13 @@ class PagesController < ApplicationController
 
   def index
     @nav_array = [
+
       { title: 'Home', icon: 'fa-home', path: '/', hover: 'hvr-buzz-out' },
       { title: 'Projects', icon: 'fa-bug', path: '/pages/projects', hover: 'hvr-buzz-out' },
       { title: 'Blog', icon: 'fa-rss', path: '/pages/blog', hover: 'hvr-pulse-grow' },
       { title: 'About', icon: 'fa-rebel', path: '/pages/about', hover: 'hvr-wobble-horizontal' },
-      { title: 'Contact', icon: 'fa-paper-plane', path: '/pages/contact', hover: 'hvr-wobble-to-top-right' }
+      { title: 'Contact', icon: 'fa-paper-plane', path: '/pages/contact', hover: 'hvr-wobble-to-top-right' },
+      { title: '', icon: '', path: '/pages/retroindex', hover: 'hvr-buzz-out' }
     ]
 
     @link_array = [
@@ -30,12 +32,12 @@ class PagesController < ApplicationController
 
   def projects
     @projects_array = [
-      { project_num: 's-1', project_name: 'ATM Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/600/600/technics', description: 'The first and the best ATM terminal ever made!', github_url: 'https://github.com/ethankhoa/CFA-ATM-terminal-app', project_cat: 'Ruby'  },
-      { project_num: 's-2', project_name: 'Recipe and Restaurant Finder', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/nightlife', description: 'Recipe finder! Yes!', github_url: 'https://github.com/ethankhoa/CFA-Recipe-Finder-App', project_cat: 'Ruby' },
-      { project_num: 's-3', project_name: 'Weather Table and Progress Bar', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/food', description: 'Ugh, not another ATM app!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
+      { project_num: 's-1', project_name: 'ATM Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/600/600/business', description: 'The first and the best ATM terminal ever made!', github_url: 'https://github.com/ethankhoa/CFA-ATM-terminal-app', project_cat: 'Ruby'  },
+      { project_num: 's-2', project_name: 'Recipe and Restaurant Finder', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/food', description: 'Recipe finder! Yes!', github_url: 'https://github.com/ethankhoa/CFA-Recipe-Finder-App', project_cat: 'Ruby' },
+      { project_num: 's-3', project_name: 'Weather Table and Progress Bar', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/nature', description: 'Ugh, not another ATM app!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
       { project_num: 's-4', project_name: 'ATM Terminal Part Deux', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/nature', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
-      { project_num: 's-5', project_name: 'ATM Terminal Part Deux', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/cats', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
-      { project_num: 's-1', project_name: 'ATM Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/600/600/technics', description: 'The first and the best ATM terminal ever made!', github_url: 'https://www.github.com', project_cat: 'Ruby'  }
+      { project_num: 's-5', project_name: 'ATM Terminal The Third', project_url: '/pages/projects', project_image: 'http://lorempixel.com/600/600/cats', description: 'This was something I did and it was super cool!', github_url: 'https://www.github.com', project_cat: 'Ruby' },
+      { project_num: 's-1', project_name: 'ATM Terminal: Rise of the Terminal', project_url: '/pages/projects-ruby-atm-terminal', project_image: 'http://lorempixel.com/600/600/technics', description: 'The first and the best ATM terminal ever made!', github_url: 'https://www.github.com', project_cat: 'Ruby'  }
                       ]
 
 end
